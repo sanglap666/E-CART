61772fda7e7807e5a3d464b33c07485b37b2b66f
sanglap666 <sanglapruskerpatra666@gmail.com>
2020-11-12 15:04:53 -0500

    updating e-cart

---
+++ update.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+
+
+
+
+if __name__ == "__main__":
+    subprocess.Popen(["git", "add", "."],stdout=subprocess.PIPE)
+
+    subprocess.Popen(["git", "status"],stdout=subprocess.PIPE)
+
+    subprocess.Popen(["git", "commit", "-m", "updating e-cart"],stdout=subprocess.PIPE)
+
+    subprocess.Popen(["git", "push", "-u", "origin", "master"],stdout=subprocess.PIPE)
